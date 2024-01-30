16efc4efcbe58b04142338ae1b74da894787e666
Vini Chimania <vini.chimania@otocapital.in>
2024-01-30 18:39:40 +0530

    integrated full design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
@@ -27,18 +27,36 @@ import {
 import { useStateContext } from "./contexts/ContextProvider";
 
 export const App = () => {
-  const { activeMenu } = useStateContext();
+  const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
+    activeMenu,
+    currentColor,
+    themeSettings,
+    setThemeSettings,
+  } = useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem("colorMode");
+    const currentThemeMode = localStorage.getItem("themeMode");
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
 
   return (
-    <div>
+    <div className={currentMode === "Dark" ? "dark" : ""}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
             <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
-                className="text-3xl p-3 hovver:drop-shodow-xl hover:bg-light-gray text-white"
-                style={{ background: "blue", borderRadius: " 50%" }}
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: "50%" }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
               </button>
@@ -60,31 +78,34 @@ export const App = () => {
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
-          </div>
-          <div>
-            <Routes>
-              <Route path="/" element={<Ecommerce />} />
-              {/* Pages */}
-              <Route path="/ecommerce" element={<Ecommerce />} />
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/employess" element={<Employees />} />
-              <Route path="/orders" element={<Orders />} />
-              <Route path="/customers" element={<Customers />} />
-              {/* Apps */}
-              <Route path="/kanban" element={<Kanban />} />
-              <Route path="/editor" element={<Editor />} />
-              <Route path="/calender" element={<Calendar />} />
-              <Route path="/color-picker" element={<ColorPicker />} />
-              {/* Charts */}
-              <Route path="/Charts/line" element={<Line />} />
-              <Route path="/Charts/area" element={<Area />} />
-              <Route path="/Charts/bar" element={<Bar />} />
-              <Route path="/Charts/pie" element={<Pie />} />
-              <Route path="/Charts/financial" element={<Financial />} />
-              <Route path="/Charts/stacked" element={<Stacked />} />
-              <Route path="/Charts/ColorMapping" element={<ColorMapping />} />
-              <Route path="/Charts/Pyramid" element={<Pyramid />} />
-            </Routes>
+            <div>
+              {themeSettings && <ThemeSettings />}
+
+              <Routes>
+                <Route path="/" element={<Ecommerce />} />
+                {/* Pages */}
+                <Route path="/ecommerce" element={<Ecommerce />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/customers" element={<Customers />} />
+                {/* Apps */}
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} />
+                {/* Charts */}
+                <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/Financial" element={<Financial />} />
+                <Route path="/stacked" element={<Stacked />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/Pyramid" element={<Pyramid />} />
+              </Routes>
+            </div>
+            <Footer />
           </div>
         </div>
       </BrowserRouter>
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
 
-const initiialState = {
+const initialState = {
   chat: false,
   cart: false,
   userProfile: false,
@@ -10,24 +10,46 @@ const initiialState = {
 };
 
 export const ContextProvider = ({ children }) => {
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState("#03C9D7");
+  const [currentMode, setCurrentMode] = useState("Light");
+  const [themeSettings, setThemeSettings] = useState(false);
   const [activeMenu, setActiveMenu] = useState(true);
-  const [isClicked, setIsClicked] = useState(initiialState);
-  const [screenSize, setScreenSize] = useState();
+  const [isClicked, setIsClicked] = useState(initialState);
 
-  const handleClick = (clicked) => {
-    setIsClicked({ ...initiialState, [clicked]: true });
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem("themeMode", e.target.value);
   };
 
+  const setColor = (color) => {
+    setCurrentColor(color);
+    localStorage.setItem("colorMode", color);
+  };
+
+  const handleClick = (clicked) =>
+    setIsClicked({ ...initialState, [clicked]: true });
+
   return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
     <StateContext.Provider
       value={{
+        currentColor,
+        currentMode,
         activeMenu,
-        setActiveMenu,
-        isClicked,
-        setIsClicked,
-        handleClick,
         screenSize,
         setScreenSize,
+        handleClick,
+        isClicked,
+        initialState,
+        setIsClicked,
+        setActiveMenu,
+        setCurrentColor,
+        setCurrentMode,
+        setMode,
+        setColor,
+        themeSettings,
+        setThemeSettings,
       }}
     >
       {children}
